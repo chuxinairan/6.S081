8ef2ff8f9176e6a42d7bc7247209c8491b3fbbae
SkyDynamicBoy <2103427676@qq.com>
2024-03-10 23:00:04 +0800

    lab3-pgtbl

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -11,10 +11,10 @@ uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +33,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,11 +44,11 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
-  
+
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -59,13 +59,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -75,22 +76,54 @@ sys_sleep(void)
   return 0;
 }
 
-
-#ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
+// #ifdef LAB_PGTBL
+int sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 va;
+  int npages;
+  uint64 ua;
+  uint64 a;
+  struct proc *p = myproc(); 
+
+  argaddr(0, &va);
+  argint(1, &npages);
+  argaddr(2, &ua);
+  uint64 bitmasks[npages/64+1];
+  uint64 mask = 1L;
+  memset(bitmasks, 0, sizeof bitmasks);
+
+  //go through PTE, unset PTE_A if set
+  uint64 downva = PGROUNDDOWN(va);
+  int index = 0;
+  pte_t *pte;
+  for(a=downva; a<downva+npages*PGSIZE; a+=PGSIZE){
+    pte = walk(p->pagetable, a, 0);
+    if(*pte & PTE_A){
+      *pte &= ~PTE_A;
+      bitmasks[index] |= mask;
+    }
+    mask <<= 1;
+    if(mask == 0){
+      index++;
+      mask = 1L;
+    }
+  }
+
+  //copy bit stream to userspace
+  if(copyout(p->pagetable, ua, (char*)bitmasks, npages/8+8) < 0){
+    return -1;
+  }
   return 0;
 }
-#endif
+// #endif
 
 uint64
 sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprintHelper(int level, pagetable_t pagetable){
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V)){
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        for(int i=level;i<=2;i++)printf(" ..");
+        PRINTPTE(i, pte);
+        uint64 child = PTE2PA(pte);
+        vmprintHelper(level-1, (pagetable_t)child);
+      } else {
+        for(int i=level;i<=2;i++)printf(" ..");
+        PRINTPTE(i, pte);
+      }
+    }
+  }
+}
+
+void 
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprintHelper(2,pagetable);
+}
