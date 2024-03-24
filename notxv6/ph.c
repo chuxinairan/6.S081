cf371efed01211c5efaf9da18af843e2d7b54ca9
yukiFi <2103427676@qq.com>
2024-03-24 16:51:20 +0800

    “lab6-thread”

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock[NBUCKET];
+
 struct entry {
   int key;
   int value;
@@ -26,6 +28,12 @@ now()
  return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
 
+void initlock(){
+  for(int i=0; i<NBUCKET; i++){
+    pthread_mutex_init(&lock[i], NULL);
+  }
+}
+
 static void 
 insert(int key, int value, struct entry **p, struct entry *n)
 {
@@ -35,6 +43,7 @@ insert(int key, int value, struct entry **p, struct entry *n)
   e->next = n;
   *p = e;
 }
+
 
 static 
 void put(int key, int value)
@@ -52,7 +61,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]); 
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]); 
   }
 
 }
@@ -105,6 +116,7 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  initlock();
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
